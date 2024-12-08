fed63bcd72938d8401c4c7fd454c5b4ad281e118
= <=>
2024-12-08 03:09:43 +0000

    added functioning menu

--- script.js
+++ script.js
@@ -3,35 +3,42 @@ window.onload = () => {
 
     menuButton.onclick = () => {
         let menuIcon = document.getElementById("menuIcon");
+        let menuBox = document.getElementById("menuBox");
+        let menuOverlay = document.getElementById("menuOverlay");
+
+        menuBox.style.display = "flex";
+        menuOverlay.style.display = "flex"
 
         // Open Menu
         if (document.getElementById("menuOverlay").classList.contains("fadeOut")) {
             menuIcon.src = "res/cancel-close-svgrepo-com.svg";
+            menuOverlay.style.zIndex = "1";
+            menuBox.style.zIndex = "2"; 
             menuOverlay.classList.add("fadeIn");
             menuOverlay.classList.remove("fadeOut");
+            menuBox.classList.add("slideIn");
+            menuBox.classList.remove("slideOut");
 
 
-        // Close Menu
+            // Close Menu
         } else {
             let menuOverlay = document.getElementById("menuOverlay");
             menuIcon.src = "res/menu-svgrepo-com.svg";
             menuOverlay.classList.remove("fadeIn");
             menuOverlay.classList.add("fadeOut");
-
-            // Wait for fadeOut transition to finish
-            menuOverlay.addEventListener("transitionend", function hideOverlay() {
-                menuOverlay.style.display = "none"; // Hide overlay after fade-out
-                menuOverlay.classList.remove("fadeOut"); // Clean up class
-                menuOverlay.removeEventListener("transitionend", hideOverlay); // Clean up the event listener
+            menuBox.classList.remove("slideIn");
+            menuBox.classList.add("slideOut");
+            setTimeout(() => { 
                 menuOverlay.style.zIndex = "-1";
-            });
+                menuBox.style.zIndex = "-1";                
+             }, 300);
         }
     };
 
     let dropdowns = document.getElementsByClassName("dropdown");
 
     for (let i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].onclick = () => { dropDownExpand(dropdowns[i].id); }; 
+        dropdowns[i].onclick = () => { dropDownExpand(dropdowns[i].id); };
     }
 
 };
